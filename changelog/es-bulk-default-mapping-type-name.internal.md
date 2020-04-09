43f448c352a51e2f22dfbda54f6aa69b7e9379e6
Reupen Shah <reupen@users.noreply.github.com>
2020-04-09 16:15:35 +0100

    Use default mapping type alias in various Elasticsearch operations
    This follows on from #2745 and changes bulk Elasticsearch indexing to use the `_doc` alias for the mapping type in place of the real mapping type name.
    
    `_doc` became an alias in Elasticsearch 6.7: https://github.com/elastic/elasticsearch/pull/39505
    
    This will allow us to seamlessly change mapping type names from their current values to `_doc` without incurring downtime.
    
    (I also took the opportunity to update various similar operations in tests.)

---
+++ changelog/es-bulk-default-mapping-type-name.internal.md
@@ -0,0 +1,1 @@
+Elasticsearch indexing operations were updated to use the default mapping type alias. This is to allow us to migrate away from using deprecated custom mapping type names.
--- datahub/cleanup/test/commands/test_delete_orphans.py
+++ datahub/cleanup/test/commands/test_delete_orphans.py
@@ -241,11 +241,10 @@ def test_run(
 
     model = apps.get_model(model_name)
     search_app = get_search_app_by_model(model)
-    doc_type = search_app.name
     read_alias = search_app.es_model.get_read_alias()
 
     assert model.objects.count() == total_model_records
-    assert es_with_signals.count(read_alias, doc_type=doc_type)['count'] == total_model_records
+    assert es_with_signals.count(read_alias)['count'] == total_model_records
 
     # Run the command
     management.call_command(command, model_name)
@@ -253,7 +252,7 @@ def test_run(
 
     # Check that the records have been deleted
     assert model.objects.count() == total_model_records - 1
-    assert es_with_signals.count(read_alias, doc_type=doc_type)['count'] == total_model_records - 1
+    assert es_with_signals.count(read_alias)['count'] == total_model_records - 1
 
     # Check which models were actually deleted
     return_values = delete_return_value_tracker.return_values
