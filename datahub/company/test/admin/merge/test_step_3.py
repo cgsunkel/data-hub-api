17772c4a4f3929209061c37d3bac07cc58613f7b
Sekhar Panja <sekharpanja@gmail.com>
2020-04-24 12:41:31 +0100

    fixup! adding pipeline item model, factory and tests

--- datahub/company/test/admin/merge/test_step_3.py
+++ datahub/company/test/admin/merge/test_step_3.py
@@ -31,7 +31,7 @@ from datahub.investment.project.models import InvestmentProject
 from datahub.investment.project.test.factories import InvestmentProjectFactory
 from datahub.omis.order.models import Order
 from datahub.omis.order.test.factories import OrderFactory
-from datahub.user.company_list.models import CompanyListItem, PipelineItem
+from datahub.user.company_list.models import CompanyListItem
 from datahub.user.company_list.test.factories import (
     CompanyListItemFactory,
     PipelineItemFactory,
