be9481105d19b2cf6ffcb79cbf3a60a2b3bdffc8
David Matthews <dwm503@gmail.com>
2020-08-14 13:11:22 +0100

    feature: add ordering to investment profiles endpoint

--- datahub/investment/investor_profile/views.py
+++ datahub/investment/investor_profile/views.py
@@ -1,4 +1,5 @@
 from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import OrderingFilter
 
 from datahub.core.viewsets import CoreViewSet
 from datahub.investment.investor_profile.models import LargeCapitalInvestorProfile
@@ -9,6 +10,8 @@ class LargeCapitalInvestorProfileViewSet(CoreViewSet):
     """Large capital investor profile view set."""
 
     serializer_class = LargeCapitalInvestorProfileSerializer
-    filter_backends = (DjangoFilterBackend,)
+    filter_backends = (DjangoFilterBackend, OrderingFilter, )
     filterset_fields = ('investor_company_id',)
+    ordering = ('-created_on')
+    ordering_fields = ('created_on', 'modified_on', 'investor_company')
     queryset = LargeCapitalInvestorProfile.objects.all()
